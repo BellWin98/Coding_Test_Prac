85d62316e1aa1b3a445b94b74aec1a4147fd9534
bellwin98 <jongseunghan@naver.com>
2022-03-04 14:05:32 +0900

    first commit

---
+++ knight.py
@@ -0,0 +1,16 @@
+input_data = input()
+row = int(input_data[1])
+column = int(ord(input_data[0])) - int(ord('a')) + 1
+
+result = 0
+
+steps = [(2, 1), (1, 2), (-2, 1), (2, -1), (-1, 2), (1, -2), (-2, -1), (-1, -2)]
+
+for step in steps:
+    new_row = row + step[0]
+    new_column = column + step[1]
+    
+    if new_row >= 1 and new_row <= 8 and new_column >= 1 and new_column <=8:
+        result += 1
+
+print(result)
