2d772cf6180913202f7efdf48dc2bc3b6459ff0f
Bellwin98 <jongseunghan@naver.com>
2022-03-20 17:04:35 +0900

    3.20 commit

--- thisIsCodingTest/prac.py
+++ thisIsCodingTest/prac.py
@@ -1,26 +1,24 @@
-def binary_search(array, target, start, end):
-    while start<=end:
-        mid = (start+end) // 2
-        if array[mid] == target:
-            return mid
-        elif array[mid] > target:
-            end = mid - 1
-        else:
-            start = mid + 1
-
-    return None
-
-n = int(input())
+n, m = map(int, input().split())
 
 array = list(map(int, input().split()))
-array.sort()
 
-m = int(input())
-x = list(map(int, input().split()))
+start = 0
+end = max(array)
 
-for i in x:
-    result = binary_search(array, i, 0, n - 1)
-    if result != None:
-        print('yes', end=' ')
+result = 0
+while start<=end:
+    total = 0
+    mid = (start+end) // 2
+    for i in array:
+        if i > mid:
+            total += i - mid
+
+    if total > m:
+        start = mid + 1
+    elif total < m:
+        end = mid - 1
     else:
-        print('no', end=' ')
+        result = mid
+        break
+
+print(result) 
