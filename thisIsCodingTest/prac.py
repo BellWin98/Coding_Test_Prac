f6aba1e1efffa9cec8793d63c1854e745a6d2416
Bellwin98 <jongseunghan@naver.com>
2022-03-21 22:48:48 +0900

    3.21 commit

--- thisIsCodingTest/prac.py
+++ thisIsCodingTest/prac.py
@@ -1,24 +1,7 @@
-n, m = map(int, input().split())
+def fibo(x):
+    if x == 1 or x == 2:
+        return 1
 
-array = list(map(int, input().split()))
+    return fibo(x-1)+fibo(x-2)
 
-start = 0
-end = max(array)
-
-result = 0
-while start<=end:
-    total = 0
-    mid = (start+end) // 2
-    for i in array:
-        if i > mid:
-            total += i - mid
-
-    if total > m:
-        start = mid + 1
-    elif total < m:
-        end = mid - 1
-    else:
-        result = mid
-        break
-
-print(result) 
+print(fibo(4))
